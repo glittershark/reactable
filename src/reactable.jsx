7a4d59cad3786aea954da929c82246c551631b38
Griffin Smith <wildgriffin45@gmail.com>
2014-04-22 13:52:01 -0600

    Fix indentation in Array.prototype.map polyfill

--- src/reactable.jsx
+++ src/reactable.jsx
@@ -4,34 +4,34 @@ Reactable = (function() {
 
     // Array.prototype.map polyfill - see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map#Polyfill
     if (!Array.prototype.map) {
-      Array.prototype.map = function(fun /*, thisArg */) {
-        "use strict";
+        Array.prototype.map = function(fun /*, thisArg */) {
+            "use strict";
 
-        if (this === void 0 || this === null) {
-          throw new TypeError();
-        }
+            if (this === void 0 || this === null) {
+                throw new TypeError();
+            }
 
-        var t = Object(this);
-        var len = t.length >>> 0;
-        if (typeof fun !== "function") {
-          throw new TypeError();
-        }
+            var t = Object(this);
+            var len = t.length >>> 0;
+            if (typeof fun !== "function") {
+                throw new TypeError();
+            }
 
-        var res = new Array(len);
-        var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
-        for (var i = 0; i < len; i++) {
-          // NOTE: Absolute correctness would demand Object.defineProperty
-          //       be used.  But this method is fairly new, and failure is
-          //       possible only if Object.prototype or Array.prototype
-          //       has a property |i| (very unlikely), so use a less-correct
-          //       but more portable alternative.
-          if (i in t) {
-            res[i] = fun.call(thisArg, t[i], i, t);
-          }
-        }
+            var res = new Array(len);
+            var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+            for (var i = 0; i < len; i++) {
+                // NOTE: Absolute correctness would demand Object.defineProperty
+                //       be used.  But this method is fairly new, and failure is
+                //       possible only if Object.prototype or Array.prototype
+                //       has a property |i| (very unlikely), so use a less-correct
+                //       but more portable alternative.
+                if (i in t) {
+                    res[i] = fun.call(thisArg, t[i], i, t);
+                }
+            }
 
-        return res;
-      };
+            return res;
+        };
     }
 
     // Array.prototype.indexOf polyfill for IE8
