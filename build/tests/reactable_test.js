0572e1709c5878eb6b2490de03fe804b77901793
Ryan Tsao <rtsao@uber.com>
2015-01-28 11:04:08 -0800

    Added test for sorting with Tr and Tds specified

--- build/tests/reactable_test.js
+++ build/tests/reactable_test.js
@@ -1026,6 +1026,72 @@ describe('Reactable', function() {
             });
         });
 
+        describe('numeric sort with Tr and Td specified', function(){
+            before(function() {
+                React.renderComponent(
+                    Reactable.Table({
+                        className: "table", 
+                        id: "table", 
+                        columns: [{ key: 'Count', sortable: Reactable.Sort.Numeric }]
+                    }, 
+                        Reactable.Tr(null, 
+                            Reactable.Td({column: "Count"}, 
+                                "23"
+                            )
+                        ), 
+                        Reactable.Tr(null, 
+                            Reactable.Td({column: "Count"}, 
+                                "18"
+                            )
+                        ), 
+                        Reactable.Tr(null, 
+                            Reactable.Td({column: "Count"}, 
+                                "28"
+                            )
+                        ), 
+                        Reactable.Tr(null, 
+                            Reactable.Td({column: "Count"}, 
+                                "1.23"
+                            )
+                        ), 
+                        Reactable.Tr(null, 
+                            Reactable.Td({column: "Count"}, 
+                                "a"
+                            )
+                        ), 
+                        Reactable.Tr(null, 
+                            Reactable.Td({column: "Count"}, 
+                                "z"
+                            )
+                        ), 
+                        Reactable.Tr(null, 
+                            Reactable.Td({column: "Count"}, 
+                                "123"
+                            )
+                        )
+                    ),
+                    ReactableTestUtils.testNode()
+                );
+            });
+
+            after(function() {
+                ReactableTestUtils.resetTestEnvironment();
+            });
+
+            it('sorts columns numerically', function(){
+                var sortHeader = $('#table thead tr.reactable-column-header th')[0];
+                ReactTestUtils.Simulate.click(sortHeader);
+
+                ReactableTestUtils.expectRowText(0, ['1.23']);
+                ReactableTestUtils.expectRowText(1, ['18']);
+                ReactableTestUtils.expectRowText(2, ['23']);
+                ReactableTestUtils.expectRowText(3, ['28']);
+                ReactableTestUtils.expectRowText(4, ['123']);
+                ReactableTestUtils.expectRowText(5, ['a']);
+                ReactableTestUtils.expectRowText(6, ['z']);
+            });
+        });
+
         describe('currency sort', function(){
             before(function() {
                 React.renderComponent(
