d02a7a1ae2d5ad3a6d71c1a3fe50d3b4733ba8e5
Tyler Sargent <tsargent@gmail.com>
2015-08-30 22:09:46 -0400

    adds pagination limiter, and previous and next buttons

--- src/reactable/paginator.jsx
+++ src/reactable/paginator.jsx
@@ -1,4 +1,33 @@
 export class Paginator {
+
+    handlePrevious() {
+        this.props.onPageChange(this.props.currentPage - 1)
+    }
+
+    handleNext() {
+        this.props.onPageChange(this.props.currentPage + 1);
+    }
+
+    handlePageButton(page) {
+        this.props.onPageChange(page);
+    }
+
+    renderPrevious() {
+        if(this.props.currentPage > 0) {
+            return <a className='reactable-previous-page' onClick={this.handlePrevious.bind(this)}>Previous</a>
+        }
+    }
+
+    renderNext() {
+        if(this.props.currentPage < this.props.numPages - 1) {
+            return <a className='reactable-next-page' onClick={this.handleNext.bind(this)}>Next</a>
+        }
+    }
+
+    renderPageButton(className, pageNum) {
+        return <a className={className} key={pageNum} onClick={this.handlePageButton.bind(this, pageNum)}>{pageNum + 1}</a>
+    }
+
     render() {
         if (typeof this.props.colSpan === 'undefined') {
             throw new TypeError('Must pass a colSpan argument to Paginator');
@@ -12,30 +41,42 @@ export class Paginator {
             throw new TypeError('Must pass a currentPage argument to Paginator');
         }
 
-        var pageButtons = [];
-        for (var i = 0; i < this.props.numPages; i++) {
-            var pageNum = i;
-            var className = "reactable-page-button";
-            if (this.props.currentPage === i) {
+        let pageButtons = [];
+        let pageButtonLimit = this.props.pageButtonLimit;
+        let currentPage = this.props.currentPage;
+        let numPages = this.props.numPages;
+        let lowerHalf = Math.round( pageButtonLimit / 2 );
+        let upperHalf = (pageButtonLimit - lowerHalf);
+
+        for (let i = 0; i < this.props.numPages; i++) {
+            let showPageButton = false;
+            let pageNum = i;
+            let className = "reactable-page-button";
+            if (currentPage === i) {
                 className += " reactable-current-page";
             }
+            pageButtons.push( this.renderPageButton(className, pageNum));
+        }
 
-            pageButtons.push(
-                <a className={className} key={i}
-                    // create function to get around for-loop closure issue
-                    onClick={(function(pageNum) {
-                        return function() {
-                            this.props.onPageChange(pageNum);
-                        }.bind(this);
-                    }.bind(this))(i)}>{i + 1}</a>
-            );
+        if(currentPage - pageButtonLimit + lowerHalf > 0) {
+            if(currentPage > numPages - lowerHalf) {
+                pageButtons.splice(0, numPages - pageButtonLimit)
+            } else {
+                pageButtons.splice(0, currentPage - pageButtonLimit + lowerHalf);
+            }
+        }
+
+        if((numPages - currentPage) > upperHalf) {
+            pageButtons.splice(pageButtonLimit, pageButtons.length - pageButtonLimit);
         }
 
         return (
             <tbody className="reactable-pagination">
                 <tr>
                     <td colSpan={this.props.colSpan}>
+                        {this.renderPrevious()}
                         {pageButtons}
+                        {this.renderNext()}
                     </td>
                 </tr>
             </tbody>
