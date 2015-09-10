abe18a6ee862208354ed13e3316c028e11bbea83
Griffin Smith <wildgriffin45@gmail.com>
2015-09-10 14:19:05 -0400

    Keep intermediate built modules around
    Rather than putting them in tmp/ and .gitignore-ing them, keep the
    individually compiled ES6 modules (each of which implicitly supports
    CommonJS and therefore Node and Webpack and friends due to UMD)
    committed to the repo.
    
    Fixes #167

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,23 +19,23 @@ module.exports = function(grunt) {
             },
             compile: {
                 files: {
-                    'tmp/reactable/lib/to_array.js': 'src/reactable/lib/to_array.jsx',
-                    'tmp/reactable/lib/filter_props_from.js': 'src/reactable/lib/filter_props_from.jsx',
-                    'tmp/reactable/lib/extract_data_from.js': 'src/reactable/lib/extract_data_from.jsx',
-                    'tmp/reactable/lib/is_react_component.js': 'src/reactable/lib/is_react_component.jsx',
-                    'tmp/reactable/lib/stringable.js': 'src/reactable/lib/stringable.jsx',
-                    'tmp/reactable/filterer.js': 'src/reactable/filterer.jsx',
-                    'tmp/reactable/sort.js': 'src/reactable/sort.jsx',
-                    'tmp/reactable/td.js': 'src/reactable/td.jsx',
-                    'tmp/reactable/tr.js': 'src/reactable/tr.jsx',
-                    'tmp/reactable/thead.js': 'src/reactable/thead.jsx',
-                    'tmp/reactable/tfoot.js': 'src/reactable/tfoot.jsx',
-                    'tmp/reactable/unsafe.js': 'src/reactable/unsafe.jsx',
-                    'tmp/reactable/th.js': 'src/reactable/th.jsx',
-                    'tmp/reactable/paginator.js': 'src/reactable/paginator.jsx',
-                    'tmp/reactable/table.js': 'src/reactable/table.jsx',
+                    'lib/reactable/lib/to_array.js': 'src/reactable/lib/to_array.jsx',
+                    'lib/reactable/lib/filter_props_from.js': 'src/reactable/lib/filter_props_from.jsx',
+                    'lib/reactable/lib/extract_data_from.js': 'src/reactable/lib/extract_data_from.jsx',
+                    'lib/reactable/lib/is_react_component.js': 'src/reactable/lib/is_react_component.jsx',
+                    'lib/reactable/lib/stringable.js': 'src/reactable/lib/stringable.jsx',
+                    'lib/reactable/filterer.js': 'src/reactable/filterer.jsx',
+                    'lib/reactable/sort.js': 'src/reactable/sort.jsx',
+                    'lib/reactable/td.js': 'src/reactable/td.jsx',
+                    'lib/reactable/tr.js': 'src/reactable/tr.jsx',
+                    'lib/reactable/thead.js': 'src/reactable/thead.jsx',
+                    'lib/reactable/tfoot.js': 'src/reactable/tfoot.jsx',
+                    'lib/reactable/unsafe.js': 'src/reactable/unsafe.jsx',
+                    'lib/reactable/th.js': 'src/reactable/th.jsx',
+                    'lib/reactable/paginator.js': 'src/reactable/paginator.jsx',
+                    'lib/reactable/table.js': 'src/reactable/table.jsx',
 
-                    'tmp/reactable.js': 'src/reactable.jsx',
+                    'lib/reactable.js': 'src/reactable.jsx',
 
                     'build/tests/reactable_test.js': 'tests/reactable_test.jsx'
                 }
@@ -44,22 +44,22 @@ module.exports = function(grunt) {
         concat: {
             dist: {
                 src: [
-                    'tmp/reactable/lib/filter_props_from.js',
-                    'tmp/reactable/lib/to_array.js',
-                    'tmp/reactable/lib/stringable.js',
-                    'tmp/reactable/lib/extract_data_from.js',
-                    'tmp/reactable/lib/is_react_component.js',
-                    'tmp/reactable/unsafe.js',
-                    'tmp/reactable/filterer.js',
-                    'tmp/reactable/sort.js',
-                    'tmp/reactable/td.js',
-                    'tmp/reactable/tr.js',
-                    'tmp/reactable/th.js',
-                    'tmp/reactable/thead.js',
-                    'tmp/reactable/tfoot.js',
-                    'tmp/reactable/paginator.js',
-                    'tmp/reactable/table.js',
-                    'tmp/reactable.js'
+                    'lib/reactable/lib/filter_props_from.js',
+                    'lib/reactable/lib/to_array.js',
+                    'lib/reactable/lib/stringable.js',
+                    'lib/reactable/lib/extract_data_from.js',
+                    'lib/reactable/lib/is_react_component.js',
+                    'lib/reactable/unsafe.js',
+                    'lib/reactable/filterer.js',
+                    'lib/reactable/sort.js',
+                    'lib/reactable/td.js',
+                    'lib/reactable/tr.js',
+                    'lib/reactable/th.js',
+                    'lib/reactable/thead.js',
+                    'lib/reactable/tfoot.js',
+                    'lib/reactable/paginator.js',
+                    'lib/reactable/table.js',
+                    'lib/reactable.js'
                 ],
                 dest: 'build/reactable.js'
             }
