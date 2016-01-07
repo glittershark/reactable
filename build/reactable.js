16c6855eb214febb71a4204b610c5069257efcce
Wout Mertens <Wout.Mertens@gmail.com>
2016-01-07 20:07:05 +0100

    build

--- build/reactable.js
+++ build/reactable.js
@@ -201,17 +201,17 @@ window.ReactDOM["default"] = window.ReactDOM;
 
 (function (global, factory) {
     if (typeof define === 'function' && define.amd) {
-        define(['exports', 'react', 'react-dom'], factory);
+        define(['exports', 'react'], factory);
     } else if (typeof exports !== 'undefined') {
-        factory(exports, require('react'), require('react-dom'));
+        factory(exports, require('react'));
     } else {
         var mod = {
             exports: {}
         };
-        factory(mod.exports, global.React, global.ReactDOM);
+        factory(mod.exports, global.React);
         global.filterer = mod.exports;
     }
-})(this, function (exports, _react, _reactDom) {
+})(this, function (exports, _react) {
     'use strict';
 
     var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
@@ -232,19 +232,21 @@ window.ReactDOM["default"] = window.ReactDOM;
         }
 
         _createClass(FiltererInput, [{
-            key: 'onChange',
-            value: function onChange() {
-                this.props.onFilter(_reactDom['default'].findDOMNode(this).value);
-            }
-        }, {
             key: 'render',
             value: function render() {
-                return _react['default'].createElement('input', { type: 'text',
+                var _props = this.props;
+                var onFilter = _props.onFilter;
+                var placeholder = _props.placeholder;
+                var value = _props.value;
+
+                return _react['default'].createElement('input', {
                     className: 'reactable-filter-input',
-                    placeholder: this.props.placeholder,
-                    value: this.props.value,
-                    onKeyUp: this.onChange.bind(this),
-                    onChange: this.onChange.bind(this) });
+                    placeholder: placeholder,
+                    value: value,
+                    onChange: function (ev) {
+                        return onFilter(ev.target.value);
+                    }
+                });
             }
         }]);
 
@@ -276,9 +278,11 @@ window.ReactDOM["default"] = window.ReactDOM;
                     _react['default'].createElement(
                         'td',
                         { colSpan: this.props.colSpan },
-                        _react['default'].createElement(FiltererInput, { onFilter: this.props.onFilter,
+                        _react['default'].createElement(FiltererInput, {
+                            onFilter: this.props.onFilter,
                             value: this.props.value,
-                            placeholder: this.props.placeholder })
+                            placeholder: this.props.placeholder
+                        })
                     )
                 );
             }
@@ -797,10 +801,10 @@ window.ReactDOM["default"] = window.ReactDOM;
 });
 
 (function (global, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(['exports', 'react'], factory);
-    } else if (typeof exports !== 'undefined') {
-        factory(exports, require('react'));
+    if (typeof define === "function" && define.amd) {
+        define(["exports", "react"], factory);
+    } else if (typeof exports !== "undefined") {
+        factory(exports, require("react"));
     } else {
         var mod = {
             exports: {}
@@ -809,18 +813,18 @@ window.ReactDOM["default"] = window.ReactDOM;
         global.paginator = mod.exports;
     }
 })(this, function (exports, _react) {
-    'use strict';
+    "use strict";
 
-    var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+    var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-    var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+    var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-    function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+    function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
     function pageHref(num) {
-        return '#page-' + (num + 1);
+        return "#page-" + (num + 1);
     }
 
     var Paginator = (function (_React$Component) {
@@ -829,59 +833,72 @@ window.ReactDOM["default"] = window.ReactDOM;
         function Paginator() {
             _classCallCheck(this, Paginator);
 
-            _get(Object.getPrototypeOf(Paginator.prototype), 'constructor', this).apply(this, arguments);
+            _get(Object.getPrototypeOf(Paginator.prototype), "constructor", this).apply(this, arguments);
         }
 
         _createClass(Paginator, [{
-            key: 'handlePrevious',
+            key: "handlePrevious",
             value: function handlePrevious(e) {
                 e.preventDefault();
                 this.props.onPageChange(this.props.currentPage - 1);
             }
         }, {
-            key: 'handleNext',
+            key: "handleNext",
             value: function handleNext(e) {
                 e.preventDefault();
                 this.props.onPageChange(this.props.currentPage + 1);
             }
         }, {
-            key: 'handlePageButton',
+            key: "handlePageButton",
             value: function handlePageButton(page, e) {
                 e.preventDefault();
                 this.props.onPageChange(page);
             }
         }, {
-            key: 'renderPrevious',
+            key: "renderPrevious",
             value: function renderPrevious() {
+                var previousBtn = {
+                    nl: "Vorige",
+                    fr: "Précédente",
+                    en: "Previous"
+                };
+                var locale = this.props.locale;
+
                 if (this.props.currentPage > 0) {
-                    return _react['default'].createElement(
-                        'a',
-                        { className: 'reactable-previous-page',
+                    return _react["default"].createElement(
+                        "a",
+                        { className: "reactable-previous-page",
                             href: pageHref(this.props.currentPage - 1),
                             onClick: this.handlePrevious.bind(this) },
-                        'Previous'
+                        previousBtn[locale] || previousBtn.en
                     );
                 }
             }
         }, {
-            key: 'renderNext',
+            key: "renderNext",
             value: function renderNext() {
+                var nextBtn = {
+                    nl: "Volgende",
+                    fr: "Suivante",
+                    en: "Next"
+                };
+                var locale = this.props.locale;
+
                 if (this.props.currentPage < this.props.numPages - 1) {
-                    return _react['default'].createElement(
-                        'a',
-                        { className: 'reactable-next-page',
+                    return _react["default"].createElement(
+                        "a",
+                        { className: "reactable-next-page",
                             href: pageHref(this.props.currentPage + 1),
                             onClick: this.handleNext.bind(this) },
-                        'Next'
+                        nextBtn[locale] || nextBtn.en
                     );
                 }
             }
         }, {
-            key: 'renderPageButton',
+            key: "renderPageButton",
             value: function renderPageButton(className, pageNum) {
-
-                return _react['default'].createElement(
-                    'a',
+                return _react["default"].createElement(
+                    "a",
                     { className: className,
                         key: pageNum,
                         href: pageHref(pageNum),
@@ -890,7 +907,7 @@ window.ReactDOM["default"] = window.ReactDOM;
                 );
             }
         }, {
-            key: 'render',
+            key: "render",
             value: function render() {
                 if (typeof this.props.colSpan === 'undefined') {
                     throw new TypeError('Must pass a colSpan argument to Paginator');
@@ -932,15 +949,14 @@ window.ReactDOM["default"] = window.ReactDOM;
                 if (numPages - currentPage > upperHalf) {
                     pageButtons.splice(pageButtonLimit, pageButtons.length - pageButtonLimit);
                 }
-
-                return _react['default'].createElement(
-                    'tbody',
-                    { className: 'reactable-pagination' },
-                    _react['default'].createElement(
-                        'tr',
+                return _react["default"].createElement(
+                    "tbody",
+                    { className: "reactable-pagination" },
+                    _react["default"].createElement(
+                        "tr",
                         null,
-                        _react['default'].createElement(
-                            'td',
+                        _react["default"].createElement(
+                            "td",
                             { colSpan: this.props.colSpan },
                             this.renderPrevious(),
                             pageButtons,
@@ -952,7 +968,7 @@ window.ReactDOM["default"] = window.ReactDOM;
         }]);
 
         return Paginator;
-    })(_react['default'].Component);
+    })(_react["default"].Component);
 
     exports.Paginator = Paginator;
     ;
@@ -1445,7 +1461,7 @@ window.ReactDOM["default"] = window.ReactDOM;
                         { className: 'reactable-data', key: 'tbody' },
                         currentChildren.length > 0 ? currentChildren : noDataText
                     ),
-                    pagination === true ? _react['default'].createElement(_paginator.Paginator, { colSpan: columns.length,
+                    pagination === true ? _react['default'].createElement(_paginator.Paginator, { locale: props.locale, colSpan: columns.length,
                         pageButtonLimit: pageButtonLimit,
                         numPages: numPages,
                         currentPage: currentPage,
@@ -1467,7 +1483,8 @@ window.ReactDOM["default"] = window.ReactDOM;
         sortBy: false,
         defaultSort: false,
         itemsPerPage: 0,
-        hideFilterInput: false
+        hideFilterInput: false,
+        locale: 'en'
     };
 });
 
