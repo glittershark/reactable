7fa70a09008238a1fbe5ccf3c87d9719886eeac8
陳昱廷 <y2468101216@gmail.com>
2016-03-08 11:30:53 +0800

    change back to PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -50,7 +50,7 @@ module.exports = function(config) {
     // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
     // - PhantomJS
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: ['Firefox'],
+    browsers: ['PhantomJS'],
 
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
