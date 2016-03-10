26ea4700647ca2881e4d0030e5a2a8b42d76c809
陳昱廷 <y2468101216@gmail.com>
2016-03-10 11:13:40 +0800

    close #243

--- tests/reactable_test.jsx
+++ tests/reactable_test.jsx
@@ -494,6 +494,27 @@ describe('Reactable', function() {
             });
         });
     });
+    
+    describe('adding <Td> with style to the <Table>', function() {
+        before(function () {
+            var tdStyle = {width:"100px"};
+            ReactDOM.render(
+                <Reactable.Table className="table" id="table">
+                    <Reactable.Tr>
+                        <Reactable.Td column="Name" className="name-1" style={tdStyle}>Griffin Smith</Reactable.Td>
+                        <Reactable.Td column="Age">18</Reactable.Td>
+                    </Reactable.Tr>       
+                </Reactable.Table>,
+                ReactableTestUtils.testNode()
+            );
+        });
+
+        after(ReactableTestUtils.resetTestEnvironment);
+
+        it('renders the first column with the width', function() {
+            expect($('td.name-1')).to.have.attr('style').match(/width/);
+        });  
+    });
 
     describe('specifying an array of columns', function() {
         describe('as strings', function() {
