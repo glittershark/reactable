aab6bcbe05bdf5ac02d5673dbac51cb008bac01a
Eric Fennell <ericf89@gmail.com>
2016-10-13 15:05:24 -0500

    Pass column props pulled from header to <Td>s.

--- build/reactable.js
+++ build/reactable.js
@@ -513,6 +513,8 @@ window.ReactDOM["default"] = window.ReactDOM;
 
     var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
+    function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
     function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
     function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
@@ -536,10 +538,14 @@ window.ReactDOM["default"] = window.ReactDOM;
                         console.log(children);
                     }
 
-                    children = children.concat(this.props.columns.map((function (column, i) {
+                    children = children.concat(this.props.columns.map((function (_ref, i) {
+                        var _ref$props = _ref.props;
+                        var props = _ref$props === undefined ? {} : _ref$props;
+
+                        var column = _objectWithoutProperties(_ref, ['props']);
+
                         if (this.props.data.hasOwnProperty(column.key)) {
                             var value = this.props.data[column.key];
-                            var props = {};
 
                             if (typeof value !== 'undefined' && value !== null && value.__reactableMeta === true) {
                                 props = value.props;
