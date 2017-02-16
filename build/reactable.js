680d6cc0b24540b7651f27df19cae3415fa630d7
Thibault Vilon <thibault@elevo.io>
2017-02-17 00:28:36 +0100

    update build files

--- build/reactable.js
+++ build/reactable.js
@@ -1455,6 +1455,10 @@ window.ReactDOM["default"] = window.ReactDOM;
                             }
                         }
 
+                        if (this.props.onRowClick) {
+                            props.onClick = this.props.onRowClick.bind(null, data);
+                        }
+
                         return _react['default'].createElement(_tr.Tr, _extends({ columns: columns, key: i, data: data }, props));
                     }).bind(this)));
                 }
